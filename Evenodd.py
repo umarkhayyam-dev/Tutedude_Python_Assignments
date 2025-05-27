bb7803a353da80713e78ecb218ccccfee762a310
umarkhayyam-dev <Umarkhayyamim21@gmail.com>
2025-05-27 13:00:33 +0530

    Add Task 1 and Task 2 Python script
    - Task 1: Check if a number is even or odd using if-else
    - Task 2: Calculate sum of integers from 1 to 50 using a for loop

---
+++ Evenodd.py
@@ -0,0 +1,5 @@
+a=int(input("Ennter a number"))
+if (a%2==0):
+    print("Entered number is even")
+else:
+    print("Entered number is odd")
---
+++ sum.py
@@ -0,0 +1,6 @@
+counter = 0
+for i in range(1, 51):
+    counter = counter + i
+print("Sum of the numbers from 1 to 50 is: ",counter)
+
+
